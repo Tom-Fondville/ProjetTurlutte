6d5d738584dfd2dbe1d0eb44aec3a57cedf0ab0d
tom-fondville <tomfondville@gmail.com>
2023-02-08 08:45:06 +0100

    init

---
+++ src/main/java/fr/epsi/turlutte/turlutte/TurlutteApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.turlutte.turlutte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurlutteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurlutteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/epsi/turlutte/turlutte/TurlutteApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.turlutte.turlutte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurlutteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
