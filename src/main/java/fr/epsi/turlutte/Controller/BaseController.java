e33ce3046114f2d04602b492d9b46660dd61af14
tom-fondville <tomfondville@gmail.com>
2023-03-13 14:03:38 +0100

    index

--- src/main/java/fr/epsi/turlutte/Controller/BaseController.java
+++ src/main/java/fr/epsi/turlutte/Controller/BaseController.java
@@ -1,4 +1,24 @@
 package fr.epsi.turlutte.Controller;
 
+
+import fr.epsi.turlutte.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 public class BaseController {
+
+    @Autowired
+    ProductService productService;
+
+
+    @GetMapping(path = "/")
+    public ModelAndView getProducts() {
+        ModelAndView mav = new ModelAndView("index_template");
+        mav.addObject("products", productService.getAllProduct());
+        return mav;
+    }
 }
