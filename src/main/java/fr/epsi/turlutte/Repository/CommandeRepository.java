77c2dc3594e51d92ca8c9bf1dab1bfe0a50ab46a
tom-fondville <tomfondville@gmail.com>
2023-03-26 17:03:30 +0200

    Commande

---
+++ src/main/java/fr/epsi/turlutte/Repository/CommandeRepository.java
@@ -0,0 +1,6 @@
+package fr.epsi.turlutte.Repository;
+
+import fr.epsi.turlutte.common.model.Commande;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommandeRepository extends CrudRepository<Commande,Long > {}
